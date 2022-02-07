55623c4d0e8d3f661bbcb797b44a640ddcf0b21a
AseelAlAseel <aseel1234500000@gmail.com>
2022-02-07 18:36:41 +0200

    add menu

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
+import React , {useState} from 'react';
+
+const menu = [
+  {
+    name:'Sushi',
+    description:'Fines fish and veggies',
+    price:22.99
+  },{
+    name:'Schnitzel',
+    description:'A german specialty',
+    price:16.50
+  },{
+    name:'Barbecue Burger',
+    description:'American, raw, meaty',
+    price:12.99
+  },{
+    name:'Green',
+    description:'Healthy...and green...',
+    price:18.99
+  },
+]
+
 const App =()=>{
+const [menu, setmenu] = useState(menu)
   return(
-    <div>
-      <h2>
-        hello
-      </h2>
-    </div>
+    <React.Fragment>
+     
+      
+    </React.Fragment>
   )
 }
 export default App;
