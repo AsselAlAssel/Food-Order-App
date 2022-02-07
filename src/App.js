e58a1cc7ffd6df59fda92a0e1b0d50651175c008
AseelAlAseel <aseel1234500000@gmail.com>
2022-02-07 13:37:14 +0200

    Initialize project using Create React App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-function App() {
-  return (
-    <div className="App">
-      <h1>hello</h1>
+const App =()=>{
+  return(
+    <div>
+      <h2>
+        hello
+      </h2>
     </div>
-  );
+  )
 }
-
 export default App;
