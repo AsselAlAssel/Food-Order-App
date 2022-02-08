08c3adaab78012e1c13fcc5df9ee00327908aee0
AseelAlAseel <aseel1234500000@gmail.com>
2022-02-08 11:54:04 +0200

    ,

--- src/App.js
+++ src/App.js
@@ -1,32 +1,37 @@
-import React , {useState} from 'react';
+import React, { useState,useContext } from "react";
 
-const menu = [
-  {
-    name:'Sushi',
-    description:'Fines fish and veggies',
-    price:22.99
-  },{
-    name:'Schnitzel',
-    description:'A german specialty',
-    price:16.50
-  },{
-    name:'Barbecue Burger',
-    description:'American, raw, meaty',
-    price:12.99
-  },{
-    name:'Green',
-    description:'Healthy...and green...',
-    price:18.99
-  },
-]
 
-const App =()=>{
-const [menu, setmenu] = useState(menu)
-  return(
+const App = () => {
+  const ctx = useContext(CartContext);
+
+  const List = [
+    {
+      name: "Sushi",
+      description: "Fines fish and veggies",
+      price: 22.99,
+    },
+    {
+      name: "Schnitzel",
+      description: "A german specialty",
+      price: 16.5,
+    },
+    {
+      name: "Barbecue Burger",
+      description: "American, raw, meaty",
+      price: 12.99,
+    },
+    {
+      name: "Green",
+      description: "Healthy...and green...",
+      price: 18.99,
+    },
+  ];
+
+  const [menu, setmenu] = useState(List);
+  return (
     <React.Fragment>
-     
-      
+    hello
     </React.Fragment>
-  )
-}
+  );
+};
 export default App;
